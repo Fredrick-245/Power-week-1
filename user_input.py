9acdaa99f90a335036f64474a48f15310175572d
fredrick <ndemofredrick245@gmail.com>
2024-05-29 11:49:27 +0300

    submit:week-1-ass

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# Ask for the name of the user
+name = input("What is your name? :");
+# Ask for the age from the user
+age = input("What is your age? ")
+# Ask for location from the user
+location = input("What is your location?")
+# Printing personalized message
+print(f"Hey {name},you are {age} years old and you live in {location}")
